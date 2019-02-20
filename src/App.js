e7ffd5f5a9e7fc2631a32d429e26e500df12acf1
Rizaldi Dev <rizaldinurm@gmail.com>
2019-02-20 16:39:31 +0700

    change add tags

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            apa artinya??
+            From feature branch
           </a>
         </header>
       </div>
